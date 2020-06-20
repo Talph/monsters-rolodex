60ca1df44a33bfb67bc1bebf46184c43b7eb4542
Tafara Shamu <talph@outlook.com>
2020-06-20 22:41:59 +0200

    adding files for github pages

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = (props) => (
     <div className="card-container">
-        <img src={`https:robohash.org/${props.monster.id}?set=set2`} alt="monster"/>
+        <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt="monster"/>
         <h2> {props.monster.name}</h2>
          <h3>{props.monster.email}</h3>
     </div>
