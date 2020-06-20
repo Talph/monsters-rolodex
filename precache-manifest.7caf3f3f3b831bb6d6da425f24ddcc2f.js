96434f35081fe41ffdec62667452c260569ff197
Tafara Shamu <talph@outlook.com>
2020-06-20 22:36:37 +0200

    Updates

---
+++ precache-manifest.7caf3f3f3b831bb6d6da425f24ddcc2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae916120352cc8d13264a0a9710dceb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4017276f00f6598e84e3",
+    "url": "/monsters-rolodex/static/css/main.d7a49591.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4017276f00f6598e84e3",
+    "url": "/monsters-rolodex/static/js/main.4f33ee87.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
